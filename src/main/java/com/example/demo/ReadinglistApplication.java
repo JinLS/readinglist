a12a296445f6c233a3de1f8e1b5b96cd7ca1996e
zhouzhiwei <jazzy147@126.com>
2017-07-01 23:45:16 +0800

    111

--- src/main/java/com/example/demo/ReadinglistApplication.java
+++ src/main/java/com/example/demo/ReadinglistApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReadinglistApplication {
-	//start main
+	//start main 111
 	public static void main(String[] args) {
 		SpringApplication.run(ReadinglistApplication.class, args);
 	}
